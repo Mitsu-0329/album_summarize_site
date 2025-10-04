903f038bef3540d82ef9e2f7f5181545ffbfd59f
EC2 Default User <ec2-user@ip-172-31-4-79.ap-southeast-2.compute.internal>
2025-10-04 00:29:02 +0000

    [Finish] 1-13

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
+
+  before_validation :family_code_check
+
+  private
+  
+  def family_code_check
+    if self.id && self.family_code.present?
+      # ファミリーが登録した時の動き
+      
+    else
+      # 新規登録の時の動き
+      loop do
+        self.family_code = SecureRandom.hex(12)
+        break unless User.find_by(family_code: self.family_code)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'posts/new'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
+  get '/homes/about' => 'homes#about', as: 'about'
 
 end
+ 
