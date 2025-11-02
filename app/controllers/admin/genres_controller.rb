236174d86d85ceab10a55dfb9c55ad99ee118d9e
EC2 Default User <ec2-user@ip-172-31-4-79.ap-southeast-2.compute.internal>
2025-11-02 11:52:36 +0000

    [Finish] 1-30

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,46 @@
+class Admin::GenresController < ApplicationController
+  layout "admin"
+  before_action :authenticate_admin!
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to genres_path
+    else
+      render :index
+    end
+  end
+
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice] = "you have updated genre successfully."
+      redirect_to genres_path
+    else
+      @genres = Genre.all
+      render :index
+    end
+  end
+
+  def destroy
+    @genre = Genre.find(params[:id])
+    genre.destroy
+    redirect_to genres_path
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:genre_name)
+  end
+
+end
